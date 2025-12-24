71655e91ff3302e83f473bb66dd1d9f00010575e
Haruni26 <aaron.seymour995@gmail.com>
2025-12-24 13:02:43 -0500

    corrected logo alt

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ import Logo from "../../public/images/logo.svg";
 export default function Header() {
   return (
     <div className="bg-blue-400/40 w-full p-6">
-      <Image src={Logo} alt="Pan Am Logo" width={190} priority />
+      <Image src={Logo} alt="Airbus Logo" width={190} priority />
     </div>
   );
 }
